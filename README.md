a8883e37a368c1b38f140d443bac1b8efefc8366
edenzhong7 <edenzhong7@qq.com>
2018-12-28 13:34:22 +0800

    ilog v0.1

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ilog
 A simple logger.
+
+### Usage
+```
+func TestILOG(t *testing.T) {
+	l := ilog.NewSimpleDefaultLogger(os.Stdout, 0, "ILOG->", true)
+	info := "INFO"
+	debug := "Debug"
+	warn := "WARN"
+	err := "ERROR"
+	fatal := "FATAL"
+	l.Info(info)
+	l.Debug(debug)
+	l.Warn(warn)
+	l.Error(err)
+	l.Fatal(fatal)
+}
+```
+
+> Output screenshot
+
+![ilog](./ilog.png)
---
+++ default_test.go
@@ -0,0 +1,51 @@
+package ilog_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/deepdive7/ilog"
+)
+
+func TestCallerInfo(t *testing.T) {
+	l := ilog.NewSimpleDefaultLogger(os.Stdout, 1, "gLogger->", true)
+	l.Error("Error")
+}
+
+// 100000	     9483 ns/op - 3083 ns/op = CallerInfo时间(6400ns)
+func BenchmarkLoggerError(b *testing.B) {
+	logFile, _ := os.Create("1.log")
+	defer logFile.Close()
+	//os.Stdout = logFile
+	l := ilog.NewSimpleDefaultLogger(logFile, 1, "gLogger->", true)
+	for i := 0; i < b.N; i++ {
+		//fmt.Println("NewSimpleDefaultLogger(logFile, 1NewSimpleDefaultLogger(logFile, 1")
+		l.Error("Error")
+	}
+}
+
+func TestILOG(t *testing.T) {
+	l := ilog.NewSimpleDefaultLogger(os.Stdout, 0, "ILOG->", true)
+	info := "INFO"
+	debug := "Debug"
+	warn := "WARN"
+	err := "ERROR"
+	fatal := "FATAL"
+	l.Info(info)
+	l.Debug(debug)
+	l.Warn(warn)
+	l.Error(err)
+	l.Fatal(fatal)
+}
+
+/*
+// 500000	      3070 ns/op
+func BenchmarkColorError(b *testing.B) {
+	f, _ := os.Create("1.log")
+	os.Stdout = f
+	s := color.New(color.FgBlack, color.BgLightCyan)
+	for i := 0; i < b.N; i++ {
+		s.Println("Logger not created correctlyLogger not created correctly")
+	}
+}
+*/
